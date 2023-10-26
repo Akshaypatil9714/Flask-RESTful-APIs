8615087eea106496f1017bfd7407d72b3038b988
Akshay Patil <113010034+Akshaypatil9714@users.noreply.github.com>
2023-10-26 15:57:56 -0700

    Update main.py
    Added the code for file post and get

--- main.py
+++ main.py
@@ -2,17 +2,25 @@ from flask import Flask, jsonify
 from extensions import db, jwt
 from auth import auth_bp
 from users import user_bp
+from upload import upload_bp
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 def create_app():
     app=Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///db.sqlite3"
     app.config['JWT_SECRET_KEY']="4c5e63801c81b198510eecf4"
 
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
     db.init_app(app)
     jwt.init_app(app)
 
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(user_bp, url_prefix='/users')
+    app.register_blueprint(upload_bp, url_prefix='/upload')
 
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_data):
